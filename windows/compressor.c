fc899fd401c9652b6c0dc2eb1eed8c209ea5bcc5
Daniel Barseghian <barseghian.daniel@gmail.com>
2026-07-17 16:14:30 +0200

    added windows support to the code

---
+++ windows/compressor.c
@@ -0,0 +1,457 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdint.h>
+#include <windows.h>
+
+typedef struct node
+{
+    unsigned char letter;
+    uint32_t repetition;
+    struct node *right;
+    struct node *left;
+} node;
+
+typedef struct pair
+{
+    node *first;
+    node *seconde;
+} pair;
+
+void write_file(unsigned char *m, unsigned char **codes, long long arr_size, char *name, node **node_arr);
+void build_codes(node *root, char *buffer, int depth, unsigned char **codes);
+const char *get_filename_ext(const char *filename);
+pair find_two_smallest(node *f_pnt[256]);
+node *create_node(char c, int freq);
+node *build_huffman(node **arr);
+void free_arr(node *arr);
+int get_value(char *num);
+void free_tree(node *t);
+
+uint32_t node_count = 0;
+
+int main (int argc, char *argv[])
+{
+    fflush(stdout);
+
+    if (argc != 2)
+    {
+        printf("usage: ./compressor file1\n");
+        return 1;
+    }
+
+    HANDLE hFile = CreateFileA(argv[1], GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile == INVALID_HANDLE_VALUE)
+    {
+        printf("open failed on %s\n", argv[1]);
+        return 1;
+    }
+
+    LARGE_INTEGER li_size;
+    if (!GetFileSizeEx(hFile, &li_size))
+    {
+        printf("GetFileSizeEx failed\n");
+        return 1;
+    }
+    long long fsize = li_size.QuadPart;
+
+    printf("size: %lli\n", fsize);
+
+    if (fsize == 0)
+    {
+        printf("file is empty\n");
+        return 1;
+    }
+
+    HANDLE hMap = CreateFileMappingA(hFile, NULL, PAGE_READONLY, 0, 0, NULL);
+    if (hMap == NULL)
+    {
+        printf("CreateFileMapping failed\n");
+        return 1;
+    }
+
+    unsigned char *m = (unsigned char *) MapViewOfFile(hMap, FILE_MAP_READ, 0, 0, fsize);
+    if (m == NULL)
+    {
+        printf("MapViewOfFile failed\n");
+        return 1;
+    }
+
+    // Create an array of frequencies and initialize it to 0
+    long long frequencies[256] = {0};
+
+    // loop until Null
+    for (long i = 0; i < fsize; i++)
+    {
+        frequencies[m[i]]++;
+    }
+
+    // Make a list of pointer to the nodes
+    node **frequencies_pnt = malloc(sizeof(node*) * 256);
+
+    // Initialize everything
+    for (int i = 0; i < 256; i++)
+    {
+        frequencies_pnt[i] = NULL;
+    }
+
+    for (int i = 0; i < 256; i++)
+    {
+        if (frequencies[i] > 0)
+        {
+            frequencies_pnt[i] = create_node((unsigned char) i, frequencies[i]);
+            node_count++;
+        }
+    }
+
+    node **node_arr = malloc(sizeof(node*) * node_count);
+
+    for (int i = 0, put = 0; i < 256 && put < node_count; i++)
+    {
+        if (frequencies_pnt[i] != NULL)
+        {
+            node_arr[put] = frequencies_pnt[i];
+            put++;
+        }
+    }
+    printf("\n");
+
+    // I need to sort node_arr
+
+    node **cpy_arr = malloc(sizeof(node*) * node_count);
+
+    for (int i = 0; i < node_count; i++)
+    {
+        cpy_arr[i] = malloc(sizeof(node));
+        *cpy_arr[i] = *node_arr[i];
+
+        for (int j = 7; j >= 0; j--)
+        {
+            printf("%d", (node_arr[i]->letter >> j) & 1);
+        }
+        printf(":%i||", node_arr[i]->repetition);
+    }
+    
+    node *t = build_huffman(node_arr);
+
+    if (t == NULL)
+    {
+        printf("error while building tree\n");
+        return 1;
+    }
+
+    char *bfr = malloc(257); // in total its 257
+    if (bfr == NULL)
+    {
+        printf("error while malloc\n");
+        return 1;
+    }
+
+    int depth = 0;
+    unsigned char **codes = calloc(256, sizeof(char *));
+    if (codes == NULL)
+    {
+        printf("error while calloc\n");
+        return 1;
+    }
+
+    if (node_count == 1)
+    {
+        codes[node_arr[0]->letter] = strdup("0");
+    }
+    else
+    {
+        build_codes(t, bfr, depth, codes);
+    }
+
+    // Get the first file extension
+    const char *first = get_filename_ext(argv[1]);
+
+    size_t size = strlen(argv[1]) + 6; // .huff + \0 = 6
+
+    char *final_name = malloc(strlen(argv[1]) + 6); // +5 for .huff + \0
+
+    snprintf(final_name, size, "output.%s.huff", first);
+
+    write_file(m, codes, fsize, final_name, cpy_arr);
+
+    for (int i = 0; i < node_count; i++)
+    {
+        free_arr(cpy_arr[i]);
+    }
+
+    free(bfr);
+    free_tree(t);
+    for (int i = 0; i < 256; i++)
+    {
+        free(codes[i]);
+    }
+    free(codes);
+    free(frequencies_pnt);
+    free(node_arr);
+    free(cpy_arr);
+
+    UnmapViewOfFile(m);
+    CloseHandle(hMap);
+    
+    CloseHandle(hFile);
+
+    printf("Successful\n");
+}
+
+void free_tree(node *t)
+{
+    if (t == NULL)
+        return;
+
+    if (t->left)
+        free_tree(t->left);
+
+    if (t->right)
+        free_tree(t->right);
+
+    free(t);
+}
+
+const char *get_filename_ext(const char *filename)
+{
+    const char *dot = strrchr(filename, '.');
+
+    if (!dot || dot == filename)
+        return "";
+
+    return dot + 1;
+}
+
+
+void free_arr(node *arr)
+{
+    if (arr->left != NULL)
+    {
+        free_arr(arr->left);
+    }
+
+    if (arr->right != NULL)
+    {
+        free_arr(arr->right);
+    }
+
+    free(arr);
+}
+
+void write_file(unsigned char *m, unsigned char **codes, long long arr_size, char *name, node **node_arr)
+{
+    int meta_byte_count = 0;
+
+    FILE *f = fopen(name, "wb");
+    if (f == NULL)
+    {
+        printf("open fail");
+        return;
+    }
+
+    // write metadata
+    // The first byte is the binary value of byte_count
+    fwrite(&node_count, sizeof(uint32_t), 1, f);
+
+    for (int i = 0; i < node_count; i++)
+    {
+        unsigned char byte_letter = (int) node_arr[i]->letter;
+        fwrite(&byte_letter, 1, 1, f);
+
+        uint32_t byte_repetition = (uint32_t) node_arr[i]->repetition;
+        fwrite(&byte_repetition, sizeof(uint32_t), 1, f);
+
+        // Seek
+        fseek(f, 0, SEEK_CUR);
+    }
+
+    // Didnt respect the primary rule, never use AI to fully write code
+    // Im going to learn bitwise operators and how to code it myself.
+
+    // Point to final so we can change it when needed
+    unsigned char final = 0;
+    int fcount = 0;
+    long long arr_index = 0;
+
+    // For every arrays
+    while (arr_index < arr_size)
+    {
+        char *code = codes[(unsigned char) m[arr_index]];
+        // For every letters
+        for (int i = 0, len = strlen(code); i < len; i++)
+        {
+            // left shift
+            final <<= 1;
+
+            // Append if there is the number
+            if (code[i] == '1')
+                final |= 1;
+
+            fcount++;
+
+            // Reset both
+            if (fcount >= 8)
+            {
+                // write
+                fwrite(&final, 1, 1, f);
+
+                // Reset count
+                fcount = 0;
+                
+                // reset final
+                final = 0;
+            }
+
+            if (i == (len - 1))
+            {
+                arr_index++;
+            }
+        }
+    }
+
+    // write remaining
+    if (fcount > 0)
+    {
+        printf("remaingings\n");
+        final <<= (8 - fcount);
+        fwrite(&final, 1, 1, f);
+    }
+
+    fclose(f);
+}
+
+void build_codes(node *root, char *buffer, int depth, unsigned char **codes)
+{
+    if (!root) return;
+
+    // LEAF
+    if (root->left == NULL && root->right == NULL)
+    {
+        buffer[depth] = '\0';
+        unsigned int index = 0;
+        index = (unsigned int) root->letter;
+        
+        codes[index] = strdup(buffer);
+        return;
+    }
+
+    // LEFT = 0
+    if (root->left)
+    {
+        buffer[depth] = '0';
+        build_codes(root->left, buffer, depth + 1, codes);
+    }
+
+    // RIGHT = 1
+    if (root->right)
+    {
+        buffer[depth] = '1';
+        build_codes(root->right, buffer, depth + 1, codes);
+    }
+}
+
+// Build a huffman tree and return the top element
+node *build_huffman(node **arr)
+{
+    while (1)
+    {
+        // Find the two smallest ones globally
+        pair values = find_two_smallest(arr);
+
+        // If there is no second node, the tree is fully built
+        if (values.seconde == NULL)
+        {
+            break; 
+        }
+        
+        int sum = values.first->repetition + values.seconde->repetition;
+
+        // Create a new parent node
+        node *parent = malloc(sizeof(node));
+        parent->repetition = sum;
+        parent->letter = 0;
+
+        parent->right = values.first;
+        parent->left = values.seconde;
+
+        int f_value = -1;
+        int s_value = -1;
+
+        // Find the matching indices in the array to update them
+        for (int j = 0; j < node_count; j++)
+        {
+            if (arr[j] == values.first && f_value == -1)
+            {
+                f_value = j;
+            }
+
+            if (arr[j] == values.seconde && s_value == -1)
+            {
+                s_value = j;
+            }
+        }
+        
+        // Remove the second node and replace the first one with the parent
+        arr[s_value] = NULL;
+        arr[f_value] = parent;
+    }
+
+    // Find and return the single remaining root node
+    for (int i = 0; i < node_count; i++)
+    {
+        if (arr[i] != NULL)
+        {
+            return arr[i];
+        }
+    }
+    
+    return NULL;
+}
+
+pair find_two_smallest(node **arr)
+{
+    pair results;
+    
+    results.first = NULL;
+    results.seconde = NULL;
+
+    for (int i = 0; i < node_count; i++)
+    {
+        // Check for null
+        if (arr[i] == NULL)
+        {
+            continue;
+        }
+
+        if (results.first == NULL || arr[i]->repetition < results.first->repetition)
+        {
+            // Swap them and put value
+            results.seconde = results.first;
+            results.first = arr[i];
+        }
+
+        else if (results.seconde == NULL || arr[i]->repetition < results.seconde->repetition)
+        {
+            results.seconde = arr[i];
+        }
+    }
+
+    return results;
+}
+
+// Build Huffman tree from frequency table
+node *create_node(char c, int freq)
+{
+    node *n = malloc(sizeof(node));
+
+    n->left = NULL;
+    n->right = NULL;
+
+    n->repetition = freq;
+    n->letter = c;
+
+    return n;
+}
---
+++ windows/decompressor.c
@@ -0,0 +1,467 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <windows.h>
+#include <io.h>
+
+typedef struct list
+{
+    unsigned char letter;
+    uint32_t repetition;
+    struct list *next;
+} list;
+
+typedef struct node
+{
+    unsigned char letter;
+    uint32_t repetition;
+    struct node *right;
+    struct node *left;
+} node;
+
+typedef struct pair
+{
+    node *first;
+    node *seconde;
+} pair;
+
+uint32_t node_count = 0;
+long long byte_count = 0;
+long long rep_length = 0;
+long after_meta = 0;
+
+int get_result(node *tree, unsigned char *payload, char *name, long long fsize);
+const char *get_filename_ext(const char *filename);
+void put_in_arr(list *l, int buffer, node **arr);
+void reverse_arr(node **arr, int len);
+void free_temp_list(list *temp_list);
+pair find_two_smallest(node **arr);
+node *build_huffman(node **arr);
+void initialize_list(list *l);
+void free_list(list *first);
+void free_tree(node *t);
+void print_all(list *l);
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./decompressor compressed\n");
+        return 1;
+    }
+
+    // Get the first file extension
+    const char *first = get_filename_ext(argv[1]);
+    if (strcmp(first, "huff"))
+    {
+        printf("has to be .huff\n");
+        return 1;
+    }
+
+    // read the metadata
+    FILE *f = fopen(argv[1], "rb");
+    if (f == NULL)
+    {
+        printf("Error while opening compressed file\n");
+        return 1;
+    }
+
+    // Put it in a char of size of one byte
+    const int BYTELEN = 8;
+
+    list *temp_list = malloc(sizeof(list));
+    initialize_list(temp_list);
+
+    // First byte is always the length
+    fread(&node_count, sizeof(uint32_t), 1, f);
+    printf("count: %" PRIu32 "\n", node_count);
+
+    unsigned char letter;
+    uint32_t rep = 0;
+
+    for (uint32_t i = 0; i < node_count && fread(&letter, sizeof(unsigned char), 1, f) == 1 &&fread(&rep, sizeof(uint32_t), 1, f) == 1; i++)
+    {
+        // Put in full repetition
+        rep_length += rep;
+
+        // Malloc 
+        list *temp = malloc(sizeof(list));
+
+        // Put the values
+        temp->letter = letter;
+        temp->repetition = rep;
+        printf("temp: %" PRIu32 "\n", rep);
+
+        // Put temporary pnt to point to the front
+        list *tmp_pnt = temp_list;
+
+        // Put the head to the temp
+        temp_list = temp;
+
+        // Put the next to the old head
+        temp_list->next = tmp_pnt;
+    }
+
+    // I cant put directly here since i can't know the count before
+    node **meta_arr = malloc(sizeof(node *) * node_count);
+    if (meta_arr == NULL)
+    {
+        printf("error while malloc\n");
+        return 1;
+    }
+
+    // Malloc every index
+    for (int i = 0; i < node_count; i++)
+    {
+        meta_arr[i] = malloc(sizeof(node));
+    }
+    
+    // Put it in the meta_arr
+    unsigned int b = 0;
+    put_in_arr(temp_list, b, meta_arr);
+
+    free_temp_list(temp_list);
+
+    // Reverse array
+    reverse_arr(meta_arr, node_count);
+    printf("%" PRIu32 "\n", node_count);
+
+    for (int i = 0; i < node_count; i++)
+    {
+        printf("%c:%i||", meta_arr[i]->letter, meta_arr[i]->repetition);
+        byte_count += meta_arr[i]->repetition;
+        after_meta = ftell(f);
+    }
+    printf("\n");
+
+    // Build the three
+    node *tree = build_huffman(meta_arr);
+
+    if (tree == NULL)
+    {
+        printf("error while building\n");
+        return 1;
+    }
+
+    fclose(f);
+
+    FILE *fb = fopen(argv[1], "rb");
+    if (fb == NULL)
+    {
+        printf("Error while reopening file\n");
+        return 1;
+    }
+
+    // Now reopen the file in byte
+    HANDLE hFile = (HANDLE)_get_osfhandle(_fileno(fb));
+
+    // Skip metadata
+    fseek(fb, after_meta, SEEK_SET);
+    long current = ftell(fb);
+
+    // get file size
+    fseek(fb, 0, SEEK_END);
+    long long end = ftell(fb);
+    fseek(fb, after_meta, SEEK_SET);
+
+    // Reed the linux variant of this since its really really easier to read.
+    if (hFile == INVALID_HANDLE_VALUE)
+    {
+        fclose(fb);
+        printf("Failed to get file handle\n");
+        return 1;
+    }
+
+    HANDLE hMap = CreateFileMappingA(hFile, NULL, PAGE_READONLY, 0, 0, NULL);
+    if (hMap == NULL)
+    {
+        fclose(fb);
+        printf("CreateFileMapping failed\n");
+        return 1;
+    }
+
+    char *filedata = (char *) MapViewOfFile(hMap, FILE_MAP_READ, 0, 0, end);
+    if (filedata == NULL)
+    {
+        CloseHandle(hMap);
+        fclose(fb);
+        printf("Map failed for filedata\n");
+        return 1;
+    }
+
+    unsigned char *payload = (unsigned char *)(filedata + after_meta); // payload of [0] is the first byte after the metadata
+
+    fclose(fb);
+
+    char *final = malloc(sizeof(argv[1]));
+    if (final == NULL)
+    {
+        printf("malloc failed\n");
+        return 1;
+    }
+
+    strcpy(final, argv[1]);
+
+    char *ext = strstr(final, ".huff"); // find where .huff start
+    if (ext != NULL)
+    {
+        *ext = '\0'; // cut the string 
+    }
+
+    int succ = get_result(tree, payload, final, (end - after_meta));
+    if (succ == 1)
+    {
+        printf("error while writting to file\n");
+        return 1;
+    }
+
+    free(meta_arr);
+
+    free_tree(tree);
+    UnmapViewOfFile(filedata);
+    CloseHandle(hMap);
+
+    printf("Successfull\n");
+
+    return 0;
+}
+
+void free_tree(node *t)
+{
+    if (t == NULL)
+        return;
+
+    if (t->left)
+        free_tree(t->left);
+
+    if (t->right)
+        free_tree(t->right);
+
+    free(t);
+}
+
+void free_temp_list(list *temp_list)
+{
+    if (temp_list->next != NULL)
+    {
+        free_temp_list(temp_list->next);
+    }
+
+    free(temp_list);
+}
+
+const char *get_filename_ext(const char *filename)
+{
+    const char *dot = strrchr(filename, '.');
+
+    if (!dot || dot == filename)
+        return "";
+
+    return dot + 1;
+}
+
+int get_result(node *tree, unsigned char *payload, char *name, long long fsize)
+{
+    FILE *fw = fopen(name, "wb");
+    if (fw == NULL)
+    {
+        printf("Opening write file failed\n");
+        return 1;
+    }
+
+    node *current = tree;
+
+    const int bytelen = 8;
+    long long byte_read = 0;
+
+    // For every nodes
+    for (long long i = 0; byte_read < rep_length && i < fsize; i++)
+    {
+        // For every letters in the byte
+        for (int j = 0; j < bytelen; j++)
+        {
+            if (byte_read >= rep_length)
+                break;
+
+            int bit = (payload[i] >> (7 - j)) & 1;
+
+            if (bit == 0)
+                current = current->left;
+
+            else if (bit == 1)
+                current = current->right;
+
+            // if its a leaf or a 0 byte
+            if (current->left == NULL && current->right == NULL)
+            {
+                fwrite(&current->letter, 1, 1, fw);
+
+                byte_read++;
+                current = tree; // reset
+            }
+        }
+    }
+
+    fclose(fw);
+    return 0;
+}
+
+void reverse_arr(node **arr, int len)
+{
+    int middle = len / 2;
+
+    for (int i = 0; i < middle; i++)
+    {
+        node *tmp = arr[i];
+        arr[i] = arr[len - 1 - i];  
+        arr[len - 1 - i] = tmp; 
+    }
+}
+
+void print_all(list *l)
+{
+    if (l == NULL)
+    {
+        return;
+    }
+
+    if (l->letter != '\0')
+    {
+        // Print metadata
+        printf("%c:%i||", l->letter, l->repetition);
+    }
+
+    if (l->next != NULL)
+    {
+        print_all(l->next);
+    }
+}
+
+/// Build a huffman tree and return the top element
+node *build_huffman(node **arr)
+{
+    while (1)
+    {
+        // Find the two smallest ones globally
+        pair values = find_two_smallest(arr);
+
+        // If there is no second node, the tree is fully built
+        if (values.seconde == NULL)
+            break;
+        
+        int sum = values.first->repetition + values.seconde->repetition;
+
+        // Create a new parent node
+        node *parent = malloc(sizeof(node));
+        parent->repetition = sum;
+        parent->letter = '\0';
+
+        parent->right = values.first;
+        parent->left = values.seconde;
+
+        int f_value = -1;
+        int s_value = -1;
+
+        // Find the matching indices in the array to update them
+        for (int j = 0; j < node_count; j++)
+        {
+            if (arr[j] == values.first && f_value == -1)
+            {
+                f_value = j;
+            }
+            if (arr[j] == values.seconde && s_value == -1)
+            {
+                s_value = j;
+            }
+        }
+        
+        // Remove the second node and replace the first one with the parent
+        arr[s_value] = NULL;
+        arr[f_value] = parent;
+    }
+
+    // Find and return the single remaining root node
+    for (int i = 0; i < node_count; i++)
+    {
+        if (arr[i] != NULL)
+        {
+            return arr[i];
+        }
+    }
+    
+    return NULL;
+}
+
+pair find_two_smallest(node **arr)
+{
+    pair results;
+    
+    results.first = NULL;
+    results.seconde = NULL;
+
+    for (int i = 0; i < node_count; i++)
+    {
+        // Check for null
+        if (arr[i] == NULL)
+        {
+            continue;
+        }
+
+        if (results.first == NULL || arr[i]->repetition < results.first->repetition)
+        {
+            // Swap them and put value
+            results.seconde = results.first;
+            results.first = arr[i];
+        }
+
+        else if (results.seconde == NULL || arr[i]->repetition < results.seconde->repetition)
+        {
+            results.seconde = arr[i];
+        }
+    }
+
+    return results;
+}
+
+void put_in_arr(list *l, int buffer, node **arr)
+{
+    // put in arr
+    arr[buffer]->letter = l->letter;
+    arr[buffer]->repetition = l->repetition;
+
+    // initialize the other values
+    arr[buffer]->right = NULL;
+    arr[buffer]->left = NULL;
+
+    // Incremente
+    buffer++;
+    
+    // do next if not null
+    if ((l->next != NULL) && (buffer < node_count))
+    {
+        put_in_arr(l->next, buffer, arr);
+    }
+}
+
+void free_list(list *first)
+{
+    if (first->next != NULL)
+    {
+        free_list(first->next);
+    }
+
+    free(first);
+}
+
+void initialize_list(list *l)
+{
+    if (l != NULL)
+    {
+        l->letter = '\0';
+        l->repetition = 0;
+        l->next = NULL;
+    }
+}
